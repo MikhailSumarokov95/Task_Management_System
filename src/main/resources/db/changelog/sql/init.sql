6adbc7dcc1515f2177954fd0c2a4683bf6c48375
MikhailSumarokov95 <ykpon1995@mail.ru>
2023-12-11 23:11:17 +0500

    refactor

--- src/main/resources/db/changelog/sql/init.sql
+++ src/main/resources/db/changelog/sql/init.sql
@@ -5,25 +5,28 @@ comment
 CASCADE;
 
 CREATE TABLE IF NOT EXISTS users(
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    email VARCHAR(64) NOT NULL,
-    password VARCHAR(64) NOT NULL,
+    id          BIGSERIAL PRIMARY KEY    NOT NULL,
+    email       VARCHAR(64)              NOT NULL,
+    password    VARCHAR(64)              NOT NULL,
     CONSTRAINT email_unique UNIQUE (email)
 );
 
 CREATE TABLE IF NOT EXISTS task(
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    title VARCHAR(64) NOT NULL,
-    description VARCHAR(1024) NOT NULL,
-    status VARCHAR(32) NOT NULL,
-    priority VARCHAR(32) NOT NULL,
-    author_id BIGSERIAL NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    executor_id BIGSERIAL REFERENCES users(id)
+    id             BIGSERIAL PRIMARY KEY    NOT NULL,
+    title          VARCHAR(64)              NOT NULL,
+    description    VARCHAR(1024)            NOT NULL,
+    status         VARCHAR(32)              NOT NULL,
+    priority       VARCHAR(32)              NOT NULL,
+    author_id      BIGSERIAL                NOT NULL
+                                            REFERENCES users(id) ON DELETE CASCADE,
+    executor_id    BIGSERIAL                REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS comment(
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    content VARCHAR(1024) NOT NULL,
-    author_id BIGSERIAL NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    task_id BIGSERIAL NOT NULL REFERENCES task(id) ON DELETE CASCADE
+    id           BIGSERIAL PRIMARY KEY    NOT NULL,
+    content      VARCHAR(1024)            NOT NULL,
+    author_id    BIGSERIAL                NOT NULL
+                                          REFERENCES users(id) ON DELETE CASCADE,
+    task_id      BIGSERIAL                NOT NULL
+                                          REFERENCES task(id) ON DELETE CASCADE
 );
